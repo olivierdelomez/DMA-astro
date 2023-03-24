d4784ea4169b0400ca3deb5c5cafe1229251fe49
Olivier Delomez <olivier.delomez@gmail.com>
2023-03-24 18:35:05 +0100

    :tada: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: { port: 3333, host: true},
+  vite: {
+    ssr: {
+      noExternal: ['@justinribeiro/lite-youtube']
+    }
+  },
+  experimental: {
+    assets: true
+   }
+});
