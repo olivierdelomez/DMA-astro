801218654628744581b079f310a630ab39dc27b4
Olivier Delomez <olivier.delomez@gmail.com>
2023-04-24 14:22:11 +0200

    :wrench: date config for post

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,7 +5,7 @@ const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    created_at: z.date().optional(),
+    created_at: z.string().optional(),
     tags: z.array(z.string()).optional(),
     image: z.object({
       url: z.string(),
