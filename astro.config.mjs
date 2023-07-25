ef3a3912d7e1583622bef95cdae8efd1f8d7dfc0
Olivier Delomez <olivier.delomez@gmail.com>
2023-07-25 11:20:46 +0200

    :package: new astro and redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,6 +18,13 @@ export default defineConfig({
   experimental: {
     assets: true
   },
+  redirects: {
+    '/silure': '/especes-defendues/silure',
+    '/anguilles': '/especes-defendues/anguille',
+    '/maigre': '/especes-defendues/maigre',
+    '/toutes-les-esp%C3%A8ces-de-poissons-marins': '/especes-defendues/poissons-marins',
+    '/classification-des-rivi%C3%A8res-%C3%A0-saumon': '/blog/classification-des-rivieres-a-saumon',
+  },
   base: '/',
   integrations: [relativeLinks(), sitemap()]
 });
