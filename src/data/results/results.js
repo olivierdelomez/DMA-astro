05040841e0b205bb904c4c7d90fef9dda38755e6
olivierdelomez <olivier.delomez@gmail.com>
2026-06-17 18:04:56 +0200

    Add judicial results for 2023-2026 to results.js and update index.astro to display them dynamically

---
+++ src/data/results/results.js
@@ -0,0 +1,283 @@
+export const results2026 = {
+    year: 2026,
+    results: [
+        {
+            title:
+                "Le Conseil d'État rejette le pourvoi de l'État contre les décisions de la Cour d'appel de Bordeaux annulant les arrêtés de pêche des poissons migrateurs dans les Landes.",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/CE/DCE_491304_20260522",
+            date: "22 mai",
+        },
+        {
+            title:
+                "Le Conseil d'État suspend l'arrêté réglementaire permanent de la pêche en Gironde.",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/CE/DCE_473765_20260522",
+            date: "22 mai",
+        },
+        {
+            title:
+                "Injonction au préfet de la Charente-Maritime d’abroger, dans un délai de deux mois, l’arrêté du 15 septembre 2022 qui autorise la pêche récréative aux filets fixes sur l'estran.",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/TA86/DTA_2303483_20260513",
+            date: "13 mai",
+        },
+        {
+            title:
+                "Annulation du mode d'instruction des licences de pêche fluviale de l'Adour - JUGEMENT 2303227 du 26-3-2026 LICENCES PÊCHE FLUVIALE 40 et JUGEMENT 2303228 du 26-3-2026 LICENCES PÊCHE FLUVIALE 64",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/TA64/DTA_2303227_20260326",
+            date: "26 mars",
+        },
+        {
+            title:
+                "Annulation de l’arrêté de pêche des poissons migrateurs en 2023 dans les Landes et dans les Pyrénées-Atlantiques: jugement 2300920 AP PÊCHE POMI 40 et jugement 2300882 AP PÊCHE POMI 64",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/TA64/DTA_2300920_20260219",
+            date: "19 février",
+        },
+        {
+            title:
+                "Transmission des captures de saumons et d'aloses déclarées dans l'Adour en 2023 par transmission 3 jours avant la clôture de la requête 2403381 et justifiant le désistement de DMA",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/TA33/ORTA_2403381_20260202",
+            date: "27 janvier",
+        },
+        {
+            title: "Publication de l'arrêté supprimant les filets dans l'Adour fluvial",
+            link: "https://www.landes.gouv.fr/contenu/telechargement/38451/313896/file/recueil-40-2026-011-recueil-des-actes-administratifs.pdf",
+            date: "12 janvier",
+        },
+    ],
+};
+
+export const results2025 = {
+    year: 2025,
+    results: [
+        {
+            title:
+                "Confirmation en appel de la suspension des travaux de bathymétrie pour l'interconnexion électrique France-Espagne. Chantier du projet «golfe de Gascogne» : la justice confirme en appel les nuisances des sondages sur les mammifères marins - Libération",
+            link: "https://www.liberation.fr/environnement/biodiversite/chantier-du-projet-golfe-de-gascogne-la-justice-confirme-en-appel-les-nuisances-des-sondages-sur-les-mammiferes-marins-20250130_LH3Y4WCYQVC2BL7Q753WZQZTQU/",
+            date: null,
+        },
+        {
+            title:
+                "Suspension partielle de l'arrêté du 6 janvier 2025 autorisant la translocation de 17 945 lamproies, aboutissant à l'interdiction de l'usage du filet dérivant en mars 2025 et à l'obligation de remettre vivantes à l'eau les lamproies fluviatiles.",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/TA33/DTA_2500154_20250131",
+            date: "31 janvier",
+        },
+        {
+            title:
+                "Suspension de l'arrêté du 31 mars 2025 complétant l’arrêté inter préfectoral du 6 janvier 2025 portant autorisation de capture et de transport de lamproies marines, interdisant la reprise de l'usage du filet dérivant.",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/TA33/ORTA_2502188_20250410",
+            date: "10 avril",
+        },
+        {
+            title:
+                "Annulation de l'arrêté du préfet de la région Nouvelle-Aquitaine du 6 juillet 2023 portant réglementation de l’usage des filets remorqués à moins de trois milles de la laisse de basse-mer du littoral du département de la Gironde.",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/TA33/DTA_2303717_20250505",
+            date: "5 mai",
+        },
+        {
+            title:
+                "Aggravation en appel de l'annulation de l'arrêté n°64-2020-11-23-011 fixant les périodes d'ouverture de la pêche en eau douce pour l'année 2021 dans le département des Pyrénées-Atlantiques.",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/CAA33/DCA_23BX01514_20250507",
+            date: "7 mai",
+        },
+        {
+            title:
+                "Aggravation en appel de l'annulation de l'arrêté n°2020-1674 fixant les conditions d'exercice de la pêche en eau douce pour l'année 2021 dans le département des Landes.",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/CAA33/DCA_23BX01518_20250507",
+            date: "7 mai",
+        },
+        {
+            title:
+                "Aggravation en appel de l'annulation de l'arrêté préfectoral n°64-2021-03-11-0000631 du 11 mars 2022 fixant les périodes d'ouverture de la pêche en eau douce pour les espèces migratrices pour l'année 2022 dans le département des Pyrénées-Atlantiques.",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/CAA33/DCA_23BX02848_20250507",
+            date: "7 mai",
+        },
+        {
+            title:
+                "Aggravation en appel de l'annulation de l'arrêté DDTM/SPEMA/2022/n°1058 du 29 juin 2022 fixant les conditions d'exercice de la pêche en eau douce en 2022 pour les espèces migratrices dans le département des Landes.",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/CAA33/DCA_23BX02884_20250507",
+            date: "7 mai",
+        },
+        {
+            title:
+                "Annulation des arrêtés de la préfecture de la région Nouvelle-Aquitaine du 28 décembre 2021 et du 18 janvier 2023 approuvant et modifiant le plan de gestion des poissons migrateurs.",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/TA33/DTA_2301077_20250515",
+            date: "15 mai",
+        },
+        {
+            title:
+                "Abrogation de l’article 5 de l’arrêté ministériel du 12 décembre 1983 fixant les conditions d’exercice de chalutage dans le pertuis breton, le pertuis d’Antioche et le courreau d’Oléron, relatives au maillage minimal autorisé pour la pêche du poisson de courreaux et de l’anguille",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/CE/DCE_459110_20250724",
+            date: "24 juillet",
+        },
+        {
+            title:
+                "Sous la pression de Défense des Milieux Aquatiques, la préfecture de Martinique adopte les définitions correctes des cours d'eau pour définir les zones de non traitement chimique, par un arrêté modificatif publié le 21 août 2025.",
+            link: null,
+            date: "4 septembre",
+        },
+        {
+            title:
+                "Rejet de l'appel du ministère de la transition écologique contre le jugement n° 2200433, 2201152 du 9 novembre 2023 annulant le plan de gestion des poissons migrateurs du bassin Garonne-Dordogne",
+            link: null,
+            date: "30 septembre",
+        },
+    ],
+};
+
+export const results2024 = {
+    year: 2024,
+    results: [
+        {
+            title:
+                "Annulation partielle de l’arrêté du 24 octobre 2023 du secrétaire d’État auprès de la Première ministre, chargé de la mer établissant des mesures spatio-temporelles visant la réduction des captures accidentelles de petits cétacés dans le golfe de Gascogne pour les années 2024, 2025, et 2026",
+            link: "https://www.conseil-etat.fr/fr/arianeweb/CE/decision/2024-12-30/489906",
+            date: "30 décembre",
+        },
+        {
+            title: "Annulation des dates de pêche 2023 de la civelle et de l'anguille jaune",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/DCE_472199_20241218",
+            date: "18 décembre",
+        },
+        {
+            title: "Annulation de la pêche maritime professionnelle des saumons, aloses et lamproies dans l'estuaire de l'Adour",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/DTA_2202040_20241108",
+            date: "8 novembre",
+        },
+        {
+            title: "Annulation de la pêche amateur au filet fixe sur l'estran girondin",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/DCA_23BX00353_20241107",
+            date: "7 novembre",
+        },
+        {
+            title: "Exclusion des filets dérivants de l'Adour fluvial par le tribunal administratif de Pau",
+            link: "/2201689.pdf",
+            date: "28 octobre",
+        },
+        {
+            title: "Suspension des travaux de l'interconnexion électrique France-Espagne pour garantir la protection des cétacés",
+            link: "/ORDO_REFERE_PENAL_ENVIRONNEMENTAL_INTERCONNEXION_ELECTRIQUE.pdf",
+            date: "14 août",
+        },
+        {
+            title:
+                "Injonction à l'État de communiquer à l'Association les quantités de poissons migrateurs (saumons, aloses et lamproies) déclarées par les pêcheurs maritimes de l’estuaire de l’Adour, sous forme des cumuls mensuels pour chacune des trois espèces et pour chacune des deux années 2020 et 2021",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/DTA_2206185_20240723",
+            date: "23 Juillet",
+        },
+        {
+            title:
+                "Annulation des articles 49 et 50 du cahier des charges pour l'exploitation du droit de pêche de l'État dans le département de la Dordogne et injonction au préfet de réviser l’article 49 afin qu’il précise que les filets de type araignée ou trémail autorisés par le code de l'environnement sont tous des filets nécessairement fixes, et l’article 50 pour qu’il mentionne expressément l’interdiction d’emploi des nasses à lamproies, dans le délai de deux mois à compter de la notification du présent jugement.",
+            link: "/blog/jugement-2300308-plagepomi-garonne",
+            date: "19 juin",
+        },
+        {
+            title: "Annulation de l'arrêté du 18 janvier 2023 modifiant le plan de gestion des poissons migrateurs de Garonne-Dordogne",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/DTA_2300301_20240530",
+            date: "30 mai",
+        },
+        {
+            title: "Interdiction des filets dérivants et des nasses à lamproies en Gironde",
+            link: "/2_JUGEMENT_2204106_16-5-2024_CCCTP_2023-2027_33_FILETS_DERIVANTS_NASSES_A_LAMPROIE.pdf",
+            date: "16 mai",
+        },
+        {
+            title:
+                "2nde annulation en appel de la pêche aux engins et filets des poissons migrateurs en Gironde, Lot-et-Garonne et Dordogne pour défaut d'évaluation des incidences Natura 2000 et violation du principe de précaution",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/DCA_22BX01814_20240409",
+            date: "9 avril",
+        },
+        {
+            title: "Suppression de 6 licences de marins-pêcheurs en amont du pont de pierre de Libourne.",
+            link: "https://opendata.justice-administrative.fr/recherche/shareFile/ORTA_2300031_20240325",
+            date: "25 mars",
+        },
+        {
+            title: "Suspension de l'arrêté inter-préfectoral du 22 janvier 2024 autorisant la translocation de 21 000 lamproies marines",
+            link: "https://bordeaux.tribunal-administratif.fr/content/download/185630/document/Ordonnance_2400891.pdf",
+            date: "26 février 2024",
+        },
+        {
+            title: "Publication de l'arrêté du 12 février 2024 abrogeant la pêche aux filets fixes dans la zone de balancement des marées sur la côte landaise",
+            link: "./7_AP_12-2-2024_ABROGEANT_FILETS_FIXES_ESTRAN_LANDAIS.pdf",
+            date: "19 février",
+        },
+        {
+            title: "Publication de l'arrêté interdisant la chasse des sept espèces d'oiseaux migrateurs sur le bassin d'Arcacho.",
+            link: "/2_AP_23-1-2024_interdisant_chasse_oiseaux_BA_2024.pdf",
+            date: "23 janvier",
+        },
+    ],
+};
+
+export const results2023 = {
+    year: 2023,
+    results: [
+        {
+            title: "Annulation des filets fixes sur l'estran de la Charente Maritime",
+            link: "/blog/jugement-2002506-filets-fixes-estran-charente-maritime",
+            date: "2 Mars",
+        },
+        {
+            title: "Injonction à l'État de fermer la pêche maritime du Golfe de Gascogne pour réduire la mortalité des dauphins",
+            link: "/blog/jugement-du-conseil-detat-n-449788-sur-la-preservation-des-dauphins",
+            date: "20 Mars",
+        },
+        {
+            title:
+                "Suspension des Plans de Gestion des poissons migrateurs des bassins Garonne-Dordogne (10/2/2023), Adour (28/3/2023) et Rhône-Méditerranée (03/04/2023)",
+            link: "/blog/jugement-2300308-plagepomi-garonne",
+            date: "10 Fevrier, 28 mars, 3 avril",
+        },
+        {
+            title: "Réduction des nouvelles dates de pêche de l'anguille en mer d'un mois 1/2",
+            link: "/blog/conseil-detat-n-472401-anguilles-jaune",
+            date: "7 Avril",
+        },
+        {
+            title: "Exclusion des chaluts des trois milles d'Arcachon et suspension du nouvel arrêté du 6 juillet 2023",
+            link: "https://www.legifrance.gouv.fr/ceta/id/CETATEXT000047439134?dateDecision=&init=true&page=1&query=%22article+L123-19-6%22+du+%22Code+de+l%27environnement%22&searchField=ALL&tab_selection=cetat",
+            date: "11 avril, 8 août",
+        },
+        {
+            title: "Suspension de la pêche de la lamproie marine dans l'Adour puis en Gironde",
+            link: "/blog/jugement-du-tribunal-administratif-de-bordeaux-n-2301662",
+            date: "17 avril",
+        },
+        {
+            title: "Fin de la chasse de 7 espèces d'oiseaux migrateurs sur le bassin d'Arcachon",
+            link: "blog/jugement-2105947-chasse-oiseaux-migrateurs-bassin-archachon",
+            date: "16 mai",
+        },
+        {
+            title: "Annulation du plan de gestion des poissons migrateurs du bassin Garonne-Dordogne",
+            link: "https://www.dalloz.fr/documentation/Document?id=TA_BORDEAUX_2023-11-09_2200433#_",
+            date: "9 novembre",
+        },
+        {
+            title: "Annulation de l'arrêté de pêche 2020 dans les Landes",
+            link: "https://www.legifrance.gouv.fr/ceta/id/CETATEXT000048478836",
+            date: "28 novembre",
+        },
+        {
+            title: "Annulation de l'arrêté de pêche permanent dans les Landes",
+            link: "https://www.legifrance.gouv.fr/ceta/id/CETATEXT000048478837",
+            date: "28 novembre",
+        },
+        {
+            title: "Suspension des dérogations à la fermeture de la pêche maritime dans le golfe de Gascogne pendant l'hiver 2023-2024",
+            link: "https://www.conseil-etat.fr/content/download/200913/document/489926-489932-489949.pdf",
+            date: "22 décembre",
+        },
+        {
+            title: "Abrogation des filets fixes amateurs sur l'estran des Landes",
+            link: "https://www.doctrine.fr/d/TA/Pau/2023/TA887BECD3FF496F7493BA",
+            date: "29 décembre",
+        },
+    ],
+};
+
+export default {
+    results2026,
+    results2025,
+    results2024,
+    results2023,
+};
